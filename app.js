66a018bbe0c5afcf58e67f3524f100eee562dc58
rayyanwaqar <rayyanwaqar101@gmail.com>
2025-12-30 09:58:29 -0800

    Add button click event to change background color

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const myButton = document.getElementById('btn');
+
+myButton.addEventListener('click', function() {
+    
+    
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+           document.body.style.backgroundColor = "#" + randomColor;
+    
+});
